a1a260b4cd22301f30cea48ba4afde0c13f2c792
Gavin-12 <72680017+Gavin-12@users.noreply.github.com>
2023-10-12 14:59:34 +1300

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,6 +7,7 @@ function Home() {
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <Link href="/SubmitArticle">Go to Submit Article</Link>
         <Link href="/ViewArticle">Go to View Articles</Link>
+        <Link href="/Moderation">Go to Moderate</Link>
       </div>
     </div>
   );
