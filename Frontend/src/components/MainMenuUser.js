f3a7ebc5349d712d2c2ab33d6da2b3fdb61a5b42
Gavin-12 <72680017+Gavin-12@users.noreply.github.com>
2023-10-22 12:31:49 +1300

    Update MainMenuUser.js

--- Frontend/src/components/MainMenuUser.js
+++ Frontend/src/components/MainMenuUser.js
@@ -10,22 +10,18 @@ class MainMenuUser extends Component {
                     <h1>Main Menu for User</h1>
                 </header>
                 <main className="menu-content">
-                    <p>Push articles to SPEED:</p>
                     <Link to="/submit-article" className="btn-option">
                         Submit Article
                     </Link>
                     
-                    <p>Search articles you pushed:</p>
                     <Link to="/search-article" className="btn-option">
                         Search for Article
                     </Link>
 
-                    <p>View approved articles by the moderator:</p>
                     <Link to="/approved" className="btn-option">
                         Approved Articles
                     </Link>
 
-                    <p>Return to options:</p>
                     <Link to="/" className="btn-option">
                         Back to Options
                     </Link>
