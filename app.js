e49e88936e013b44720d2168304a6646eba12a0f
Gavin-12 <72680017+Gavin-12@users.noreply.github.com>
2023-10-22 14:09:25 +1300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,41 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+require("dotenv").config({ path: "./config.env"});
+const path = require('path');
+const express = require('express');
+const connectDB = require('./config/db');
+var cors = require('cors');
+const users = require('./routes/api/users');
+const articles = require('./routes/api/articles');
 
-dotenv.config();
+const app = express();
 
-const dbUri = process.env.MONGODB_URI;
+// Connect Database
+connectDB();
 
-mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB Connected'))
-  .catch(err => console.log(err));
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// use Routes
+app.use('/api/articles', articles);
+app.use('/api/users', users);
+
+app.use(express.static(__dirname+'/Frontend/build'));
+app.get('*', (req, res) => {res.sendFile(__dirname+'/Frontend/build/index.html')});
+
+if(process.env.NODE_ENV === "production")
+{
+    app.use(express.static(path.join(__dirname,"/Frontend/build")))
+
+    app.get("*",(req,res) =>{
+      res.sendFile(path.join(__dirname,"Frontend","build","index.html"));
+    });
+}else{
+    app.get("/",(req,res) =>{
+        res.send("Api running");
+    });
+}
+const port = process.env.PORT || 8082;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
